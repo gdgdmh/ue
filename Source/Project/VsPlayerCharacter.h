088564a44872dfe4678f7ad6bfbafd80aa7ce13b
gdgdmh <gdgdkuro@gmail.com>
2023-01-30 21:29:10 +0900

    VsCharacterを追加

--- Source/Project/VsPlayerCharacter.h
+++ Source/Project/VsPlayerCharacter.h
@@ -19,6 +19,46 @@ public:
 
 protected:
 
+    // To add mapping context
+    virtual void BeginPlay() override;
+
+    /** Called for movement input */
+    virtual void Move(const FInputActionValue& Value) override;
+
+    /** Called for looking input */
+    virtual void Look(const FInputActionValue& Value) override;
+
+    // APawn interface
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+#if 0
+protected:
+    /** Camera boom positioning the camera behind the character */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+        class USpringArmComponent* CameraBoom;
+
+    /** Follow camera */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+        class UCameraComponent* FollowCamera;
+
+    /** MappingContext */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+        class UInputMappingContext* DefaultMappingContext;
+
+    /** Jump Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+        class UInputAction* JumpAction;
+
+    /** Move Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+        class UInputAction* MoveAction;
+
+    /** Look Input Action */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+        class UInputAction* LookAction;
+#endif
+protected:
+
     //ProjectCharacterÇ∆íËã`Ç©Ç‘ÇËÇµÇƒÇ¢ÇÈÇÃÇ≈Ç¢Ç¡ÇΩÇÒè¡Ç∑
 #if 0
     UPROPERTY(EditDefaultsOnly, Category = "Gameplay|Weapon")
