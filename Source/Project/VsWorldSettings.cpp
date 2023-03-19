9b93133391b0a87ca9e97c47a5686afd21e0f199
gdgdmh <gdgdkuro@gmail.com>
2023-03-19 10:54:56 +0900

    敵のSpawnサブシステムが必ず生成されていたのを修正

---
+++ Source/Project/VsWorldSettings.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VsWorldSettings.h"
+
+AVsWorldSettings::AVsWorldSettings()
+{
+	bUseEnemySpawnSubsystem = false;
+}
