2108f38e976ebb33b9583b78abcdc1acafc85e17
gdgdmh <gdgdkuro@gmail.com>
2023-04-23 15:45:21 +0900

    UserWidgetをリネーム

---
+++ Source/Project/VsUserWidget.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CPPVsUserWidgetType.h"
+#include "VsUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API UVsUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UVsUserWidget(const FObjectInitializer& ObjectInitializer);
+
+public:
+	void SetWidgetType(EVsUserWidgetType Type) { VsUserWidgetType = Type; }
+	EVsUserWidgetType GetWidgetType() const { return VsUserWidgetType; }
+
+private:
+	EVsUserWidgetType VsUserWidgetType = EVsUserWidgetType::None;
+};
