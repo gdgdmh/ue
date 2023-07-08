15ff55f20b88b09be774d6246536ae99002baec8
gdgdmh <gdgdkuro@gmail.com>
2023-07-08 19:36:26 +0900

    RPG用のWBPを追加

---
+++ Source/Project/Rpg/RpgCharaInfo.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../ProjectUserWidget.h"
+#include "RpgCharaInfo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API URpgCharaInfo : public UProjectUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+
+
+public:
+	
+	UPROPERTY(BlueprintReadWrite)
+		FText NameText;
+
+	UPROPERTY(BlueprintReadWrite)
+		FText HpText;
+
+	UPROPERTY(BlueprintReadWrite)
+		FText SpText;
+
+	UPROPERTY(BlueprintReadWrite)
+		FText StText;
+};
