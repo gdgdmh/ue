1a2e8fb2704851bf84878955b359b0dbbea85cdf
gdgdmh <gdgdkuro@gmail.com>
2023-07-29 11:37:58 +0900

    敵のキャラクタークラスの追加

---
+++ Source/Project/Rpg/RpgBattleCharacterEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RpgBattleCharacter.h"
+#include "RpgBattleCharacterEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API URpgBattleCharacterEnemy : public URpgBattleCharacter
+{
+	GENERATED_BODY()
+	
+};
