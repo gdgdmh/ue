21fd04ed1d6c30de165dd16fa30245a203beed1b
gdgdmh <gdgdkuro@gmail.com>
2023-08-24 15:23:42 +0900

    CardのWidgetを追加

---
+++ Source/Project/Rpg/RpgCardUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../ProjectUserWidget.h"
+#include "RpgCardUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API URpgCardUserWidget : public UProjectUserWidget
+{
+	GENERATED_BODY()
+	
+};
