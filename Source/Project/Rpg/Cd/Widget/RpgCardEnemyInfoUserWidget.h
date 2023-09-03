f852a5ccf19d0732e48a9e6d3226e016317a7e52
gdgdmh <gdgdkuro@gmail.com>
2023-09-03 15:02:41 +0900

    RpgCardEnemyInfoの関連付け

---
+++ Source/Project/Rpg/Cd/Widget/RpgCardEnemyInfoUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../../../ProjectUserWidget.h"
+#include "RpgCardEnemyInfoUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API URpgCardEnemyInfoUserWidget : public UProjectUserWidget
+{
+	GENERATED_BODY()
+	
+};
