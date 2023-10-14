ab54fd5e3d4a4b7531d5985cfc994781bf6ff0f6
gdgdmh <gdgdkuro@gmail.com>
2023-10-14 21:36:35 +0900

    DataTableユーティリティーを作成

---
+++ Source/Project/Common/DataTableUtility.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataTableUtility.h"
+
+template<typename T>
+DataTableUtility::LoadStatus DataTableUtility::LoadDataTable(TArray<T>& Datas, const FString& DataTableReferencePath)
+{
+	Datas.Empty();
+	TObjectPtr<T> DataTable = LoadObject<T>(nullptr, *DataTableReferencePath, nullptr, LOAD_None, nullptr);
+	if (!DataTable)
+	{
+		// パスが間違っている？
+		return LoadStatus::FailureLoadObject;
+	}
+
+	// データ取得
+	TArray<FName> RowArray = DataTable->GetRowNames();
+	if (RowArray.IsEmpty())
+	{
+		// データテーブルにそもそもデータがない
+		return LoadStatus::FailureEmptyData;
+	}
+
+	for (const FName RowName : RowArray)
+	{
+		auto TempTable = DataTable->FindRow<T>(RowName, FString());
+		if (!TempTable)
+		{
+			// 型が異なっている？
+			return LoadStatus::FailureTableType;
+		}
+		// データに追加
+		Datas.Add(*TempTable);
+	}
+	// 1件以上データ追加できているので成功
+	return LoadStatus::Success;
+}
+
---
+++ Source/Project/Common/DataTableUtility.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECT_API DataTableUtility
+{
+public:
+	/// <summary>
+	/// 読み込みステータス
+	/// </summary>
+	enum class LoadStatus : int32
+	{
+		Success,
+		FailureLoadObject,		// LoadObjectに失敗(おそらくリファレンスパスが間違っている)
+		FailureEmptyData,		// DataTableが空
+		FailureTableType,		// テンプレートに指定された型と実際のデータテーブルの方が異なっている？
+		Failure
+	};
+public:
+
+	/// <summary>
+	/// テーブル読み込み
+	/// </summary>
+	/// <typeparam name="T">データ定義した構造体</typeparam>
+	/// <param name="Datas">データを格納する配列</param>
+	/// <param name="DataTableReferencePath">対象データテーブルのリファレンスパス</param>
+	/// <returns>読み込み結果</returns>
+	template<typename T>
+	static LoadStatus LoadDataTable(TArray<T>& Datas, const FString& DataTableReferencePath);
+
+
+};
