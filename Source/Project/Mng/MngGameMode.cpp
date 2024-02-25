c148700ce3fcaba55739897353c8e24487cad62b
gdgdmh <gdgdkuro@gmail.com>
2024-02-25 16:10:36 +0900

    ミニゲーム用のゲームモードを作成

---
+++ Source/Project/Mng/MngGameMode.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MngGameMode.h"
+
+AMngGameMode::AMngGameMode(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+	DefaultPawnClass = APawn::StaticClass();
+}
+
+void AMngGameMode::BeginPlay()
+{
+	UE_LOG(LogTemp, Log, TEXT("AMngGameMode::BeginPlay"));
+	Super::BeginPlay();
+}
---
+++ Source/Project/Mng/MngGameMode.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MngGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API AMngGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AMngGameMode(const FObjectInitializer& ObjectInitializer);
+
+	virtual void BeginPlay() override;
+
+};
